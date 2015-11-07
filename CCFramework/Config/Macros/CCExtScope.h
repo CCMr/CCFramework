c04b1c9f623099c3480ada1c0cd6c68e7f17ee23
gmaukh4517 <gmaukh4517@163.com>
2015-11-07 17:24:18 +0800

    弱强引用宏

---
+++ CCFramework/Config/Macros/CCExtScope.h
@@ -0,0 +1,151 @@
+//
+//  CCExtScope.h
+//  CCFramework
+//
+// Copyright (c) 2015 CC ( http://www.ccskill.com )
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#import "CCmetamacros.h"
+
+/**
+ * \@onExit defines some code to be executed when the current scope exits. The
+ * code must be enclosed in braces and terminated with a semicolon, and will be
+ * executed regardless of how the scope is exited, including from exceptions,
+ * \c goto, \c return, \c break, and \c continue.
+ *
+ * Provided code will go into a block to be executed later. Keep this in mind as
+ * it pertains to memory management, restrictions on assignment, etc. Because
+ * the code is used within a block, \c return is a legal (though perhaps
+ * confusing) way to exit the cleanup block early.
+ *
+ * Multiple \@onExit statements in the same scope are executed in reverse
+ * lexical order. This helps when pairing resource acquisition with \@onExit
+ * statements, as it guarantees teardown in the opposite order of acquisition.
+ *
+ * @note This statement cannot be used within scopes defined without braces
+ * (like a one line \c if). In practice, this is not an issue, since \@onExit is
+ * a useless construct in such a case anyways.
+ */
+#define onExit         \
+autoreleasepool {} \
+__strong cc_cleanupBlock_t metamacro_concat(cc_exitBlock_, __LINE__) __attribute__((cleanup(cc_executeCleanupBlock), unused)) = ^
+
+/**
+ * Creates \c __weak shadow variables for each of the variables provided as
+ * arguments, which can later be made strong again with #strongify.
+ *
+ * This is typically used to weakly reference variables in a block, but then
+ * ensure that the variables stay alive during the actual execution of the block
+ * (if they were live upon entry).
+ *
+ * See #strongify for an example of usage.
+ */
+#define weakify(...)   \
+autoreleasepool {} \
+metamacro_foreach_cxt(cc_weakify_, , __weak, __VA_ARGS__)
+
+/**
+ * Like #weakify, but uses \c __unsafe_unretained instead, for targets or
+ * classes that do not support weak references.
+ */
+#define unsafeify(...) \
+autoreleasepool {} \
+metamacro_foreach_cxt(ccc_weakify_, , __unsafe_unretained, __VA_ARGS__)
+
+/**
+ * Strongly references each of the variables provided as arguments, which must
+ * have previously been passed to #weakify.
+ *
+ * The strong references created will shadow the original variable names, such
+ * that the original names can be used without issue (and a significantly
+ * reduced risk of retain cycles) in the current scope.
+ *
+ * @code
+ 
+ id foo = [[NSObject alloc] init];
+ id bar = [[NSObject alloc] init];
+ 
+ @weakify(foo, bar);
+ 
+ // this block will not keep 'foo' or 'bar' alive
+ BOOL (^matchesFooOrBar)(id) = ^ BOOL (id obj){
+ // but now, upon entry, 'foo' and 'bar' will stay alive until the block has
+ // finished executing
+ @strongify(foo, bar);
+ 
+ return [foo isEqual:obj] || [bar isEqual:obj];
+ };
+ 
+ * @endcode
+ */
+#define strongify(...)                                      \
+try {                                                   \
+} @finally {                                            \
+}                                                       \
+_Pragma("clang diagnostic push")                        \
+_Pragma("clang diagnostic ignored \"-Wshadow\"")    \
+metamacro_foreach(cc_strongify_, , __VA_ARGS__) \
+_Pragma("clang diagnostic pop")
+
+/*** implementation details follow ***/
+typedef void (^cc_cleanupBlock_t)();
+
+static inline void cc_executeCleanupBlock(__strong cc_cleanupBlock_t *block)
+{
+    (*block)();
+}
+
+#define cc_weakify_(INDEX, CONTEXT, VAR) \
+CONTEXT __typeof__(VAR) metamacro_concat(VAR, _weak_) = (VAR);
+
+#define cc_strongify_(INDEX, VAR) \
+__strong __typeof__(VAR) VAR = metamacro_concat(VAR, _weak_);
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark - 全局变量与方法
+/**
+ *  @author CC, 2015-08-13
+ *
+ *  @brief  弱引用对象
+ *
+ *  @param self 当前页面对象
+ *
+ *  @return 弱引用定义
+ *
+ *  @since 1.0
+ */
+#define WEAKSELF typeof(self) __weak weakSelf = self
+
+/**
+ *  @author CC, 2015-08-13
+ *
+ *  @brief  强类型弱引用
+ *
+ *  @param weakSelf 弱引用对象
+ *
+ *  @return 强类型引用定义
+ *
+ *  @since 1.0
+ */
+#define STRONGSELF __strong __typeof(weakSelf) strongSelf = weakSelf
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
