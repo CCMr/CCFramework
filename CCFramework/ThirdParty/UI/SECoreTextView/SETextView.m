054a07bc1ef4a2bda914ca7c494983a93e4bfe37
gmaukh4517 <gmaukh4517@163.com>
2015-11-23 17:46:58 +0800

    细节调整

--- CCFramework/ThirdParty/UI/SECoreTextView/SETextView.m
+++ CCFramework/ThirdParty/UI/SECoreTextView/SETextView.m
@@ -21,7 +21,6 @@
 #import "SETextGeometry.h"
 #import "SEConstants.h"
 #import "SETextAttachment.h"
-#import "SELinkText.h"
 
 typedef NS_ENUM (NSUInteger, SETouchPhase) {
     SETouchPhaseNone       = 0,
