11ebfb930d081b38e2ff5a788d92e4b36c2674a4
gmaukh4517 <gmaukh4517@163.com>
2016-02-24 15:49:50 +0800

    调整滑动效果

--- CCFramework/ThirdParty/UI/CCPhotoPicker/Views/CCPhotoPreviewCell.m
+++ CCFramework/ThirdParty/UI/CCPhotoPicker/Views/CCPhotoPreviewCell.m
@@ -24,20 +24,21 @@
 //
 
 #import "CCPhotoPreviewCell.h"
-
+#import "config.h"
 #import "CCAssetModel.h"
 
 @interface CCPhotoPreviewCell () <UIScrollViewDelegate>
 
-@property (nonatomic, strong) UIScrollView *scrollView;
-@property (nonatomic, strong) UIView *containerView;
-@property (nonatomic, strong) UIImageView *imageView;
+@property(nonatomic, strong) UIScrollView *scrollView;
+@property(nonatomic, strong) UIView *containerView;
+@property(nonatomic, strong) UIImageView *imageView;
 
 @end
 
 @implementation CCPhotoPreviewCell
 
-- (instancetype)initWithFrame:(CGRect)frame {
+- (instancetype)initWithFrame:(CGRect)frame
+{
     if (self = [super initWithFrame:frame]) {
         [self _setup];
     }
@@ -46,15 +47,16 @@
 
 #pragma mark - Methods
 
-- (void)configCellWithItem:(CCAssetModel *)item {
+- (void)configCellWithItem:(CCAssetModel *)item
+{
     
-//    [self.scrollView setZoomScale:1.0f];
+    [self.scrollView setZoomScale:1.0f];
     self.imageView.image = item.previewImage;
     [self _resizeSubviews];
-    
 }
 
-- (void)_setup {
+- (void)_setup
+{
     
     self.backgroundColor = self.contentView.backgroundColor = [UIColor blackColor];
     
@@ -70,30 +72,30 @@
     [singleTap requireGestureRecognizerToFail:doubleTap];
     [self.contentView addGestureRecognizer:singleTap];
     [self.contentView addGestureRecognizer:doubleTap];
-    
 }
 
-- (void)_resizeSubviews {
+- (void)_resizeSubviews
+{
     self.containerView.frame = self.bounds;
     UIImage *image = self.imageView.image;
-    if (!image) {
+    if (!image)
         return;
-    }
+    
     CGFloat screenScale = [UIScreen mainScreen].scale;
-    CGFloat widthPercent = (image.size.width / screenScale) / self.frame.size.width;
-    CGFloat heightPercent = (image.size.height / screenScale) / self.frame.size.height;
+    CGFloat widthPercent = (image.size.width / screenScale) / winsize.width;
+    CGFloat heightPercent = (image.size.height / screenScale) / winsize.height;
     if (widthPercent <= 1.0f && heightPercent <= 1.0f) {
-        self.containerView.bounds = CGRectMake(0, 0, image.size.width/screenScale, image.size.height/screenScale);
-        self.containerView.center = CGPointMake(self.bounds.size.width/2, self.bounds.size.height/2);
+        self.containerView.bounds = CGRectMake(0, 0, image.size.width / screenScale, image.size.height / screenScale);
+        self.containerView.center = CGPointMake(self.bounds.size.width / 2, self.bounds.size.height / 2);
     } else if (widthPercent > 1.0f && heightPercent < 1.0f) {
         self.containerView.frame = CGRectMake(0, 0, self.frame.size.width, heightPercent * self.frame.size.width);
-    }else if (widthPercent <= 1.0f && heightPercent > 1.0f) {
-        self.containerView.frame = CGRectMake(0, 0, self.frame.size.height * widthPercent ,self.frame.size.height);
-    }else {
+    } else if (widthPercent <= 1.0f && heightPercent > 1.0f) {
+        self.containerView.frame = CGRectMake(0, 0, self.frame.size.height * widthPercent, self.frame.size.height);
+    } else {
         if (widthPercent > heightPercent) {
             self.containerView.frame = CGRectMake(0, 0, self.frame.size.width, heightPercent * self.frame.size.width);
-        }else {
-            self.containerView.frame = CGRectMake(0, 0, self.frame.size.height * widthPercent ,self.frame.size.height);
+        } else {
+            self.containerView.frame = CGRectMake(0, 0, self.frame.size.height * widthPercent, self.frame.size.height);
         }
     }
     
@@ -101,34 +103,35 @@
     [self.scrollView scrollRectToVisible:self.bounds animated:NO];
     self.scrollView.alwaysBounceVertical = self.containerView.frame.size.height <= self.frame.size.height ? NO : YES;
     self.imageView.frame = self.containerView.bounds;
-    
 }
 
-
-- (void)_handleSingleTap {
+- (void)_handleSingleTap
+{
     self.singleTapBlock ? self.singleTapBlock() : nil;
 }
 
-- (void)_handleDoubleTap:(UITapGestureRecognizer *)doubleTap {
+- (void)_handleDoubleTap:(UITapGestureRecognizer *)doubleTap
+{
     if (self.scrollView.zoomScale > 1.0f) {
         [self.scrollView setZoomScale:1.0 animated:YES];
-    }else {
+    } else {
         CGPoint touchPoint = [doubleTap locationInView:self.imageView];
         CGFloat newZoomScale = self.scrollView.maximumZoomScale;
         CGFloat xsize = self.frame.size.width / newZoomScale;
         CGFloat ysize = self.frame.size.height / newZoomScale;
-        [self.scrollView zoomToRect:CGRectMake(touchPoint.x - xsize/2, touchPoint.y - ysize/2, xsize, ysize) animated:YES];
+        [self.scrollView zoomToRect:CGRectMake(touchPoint.x - xsize / 2, touchPoint.y - ysize / 2, xsize, ysize) animated:YES];
     }
 }
 
-
 #pragma mark - UIScrollViewDelegate
 
-- (nullable UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+- (nullable UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
     return self.containerView;
 }
 
-- (void)scrollViewDidZoom:(UIScrollView *)scrollView {
+- (void)scrollViewDidZoom:(UIScrollView *)scrollView
+{
     CGFloat offsetX = (scrollView.frame.size.width > scrollView.contentSize.width) ? (scrollView.frame.size.width - scrollView.contentSize.width) * 0.5 : 0.0;
     CGFloat offsetY = (scrollView.frame.size.height > scrollView.contentSize.height) ? (scrollView.frame.size.height - scrollView.contentSize.height) * 0.5 : 0.0;
     self.containerView.center = CGPointMake(scrollView.contentSize.width * 0.5 + offsetX, scrollView.contentSize.height * 0.5 + offsetY);
@@ -136,7 +139,8 @@
 
 #pragma mark - Getters
 
-- (UIScrollView *)scrollView {
+- (UIScrollView *)scrollView
+{
     if (!_scrollView) {
         _scrollView = [[UIScrollView alloc] initWithFrame:self.bounds];
         _scrollView.bouncesZoom = YES;
@@ -156,7 +160,8 @@
 }
 
 
-- (UIView *)containerView {
+- (UIView *)containerView
+{
     if (!_containerView) {
         _containerView = [[UIView alloc] initWithFrame:self.bounds];
         _containerView.clipsToBounds = YES;
@@ -164,7 +169,8 @@
     return _containerView;
 }
 
-- (UIImageView *)imageView {
+- (UIImageView *)imageView
+{
     if (!_imageView) {
         _imageView = [[UIImageView alloc] initWithFrame:self.bounds];
         _imageView.backgroundColor = [UIColor colorWithWhite:1.000 alpha:0.500];
--- CCFramework/ThirdParty/UI/CCPhotoPicker/Views/CCPhotoPreviewViewLayout.m
+++ CCFramework/ThirdParty/UI/CCPhotoPicker/Views/CCPhotoPreviewViewLayout.m
@@ -23,9 +23,10 @@
 // THE SOFTWARE.
 //
 
-
 #import "CCPhotoPreviewViewLayout.h"
 #import "config.h"
+
+#define ACTIVE_DISTANCE 200
 
 @implementation CCPhotoPreviewViewLayout
 
@@ -43,6 +44,75 @@
 - (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)oldBounds
 {
     return YES;
+}
+
+//最大旋转角度
+#define rotate 35.0 * M_PI / 180.0
+//返回一个rect位置下所有cell的位置数组
+- (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect
+{
+    //得到所有的UICollectionViewLayoutAttributes
+    NSArray *array = [super layoutAttributesForElementsInRect:rect];
+    
+    CGRect visibleRect;
+    visibleRect.origin = self.collectionView.contentOffset;
+    visibleRect.size = self.collectionView.bounds.size;
+    
+    for (UICollectionViewLayoutAttributes *attributes in array) {
+        //cell中心离collectionview中心的位移
+        //CGRectGetMidX表示得到一个frame中心点的X坐标
+        CGFloat distance = CGRectGetMidX(visibleRect) - attributes.center.x;
+        //CGRectIntersectsRect 判断两个矩形是否相交
+        //这里判断当前这个cell在不在rect矩形里
+        if (CGRectIntersectsRect(attributes.frame, rect)) {
+            CGFloat normalizedDistance = distance / ACTIVE_DISTANCE;
+            //ABS 绝对值
+            //如果位移小于一个过程所需的位移
+            if (ABS(distance) < ACTIVE_DISTANCE) {
+                //normalizedDistance 当前位移比上完成一个过程所需位移 得到不完全过程的旋转角度
+                CGFloat zoom = rotate * normalizedDistance;
+                CATransform3D transfrom = CATransform3DIdentity;
+                transfrom.m34 = 1.0 / 600;
+                transfrom = CATransform3DRotate(transfrom, -zoom, 0.0f, 1.0f, 0.0f);
+                attributes.transform3D = transfrom;
+                attributes.zIndex = 1;
+            } else {
+                CATransform3D transfrom = CATransform3DIdentity;
+                transfrom.m34 = 1.0 / 600;
+                
+                if (distance > 0) { //向右滑
+                    transfrom = CATransform3DRotate(transfrom, -rotate, 0.0f, 1.0f, 0.0f);
+                } else { //向左滑
+                    transfrom = CATransform3DRotate(transfrom, rotate, 0.0f, 1.0f, 0.0f);
+                }
+                
+                attributes.transform3D = transfrom;
+                attributes.zIndex = 1;
+            }
+        }
+    }
+    return array;
+}
+
+//类似于scrollview的scrollViewWillEndDragging
+//proposedContentOffset是没有对齐到网格时本来应该停下的位置
+- (CGPoint)targetContentOffsetForProposedContentOffset:(CGPoint)proposedContentOffset withScrollingVelocity:(CGPoint)velocity
+{
+    CGFloat offsetAdjustment = MAXFLOAT;
+    //CGRectGetWidth: 返回矩形的宽度
+    CGFloat horizontalCenter = proposedContentOffset.x + (CGRectGetWidth(self.collectionView.bounds) / 2.0);
+    //当前rect
+    CGRect targetRect = CGRectMake(proposedContentOffset.x, 0, self.collectionView.bounds.size.width, self.collectionView.bounds.size.height);
+    
+    NSArray *array = [super layoutAttributesForElementsInRect:targetRect];
+    //对当前屏幕中的UICollectionViewLayoutAttributes逐个与屏幕中心进行比较，找出最接近中心的一个
+    for (UICollectionViewLayoutAttributes *layoutAttributes in array) {
+        CGFloat itemHorizontalCenter = layoutAttributes.center.x;
+        if (ABS(itemHorizontalCenter - horizontalCenter) < ABS(offsetAdjustment))//与中心的位移差
+            offsetAdjustment = itemHorizontalCenter - horizontalCenter;
+    }
+    //返回修改后停下的位置
+    return CGPointMake(proposedContentOffset.x + offsetAdjustment, proposedContentOffset.y);
 }
 
 @end
